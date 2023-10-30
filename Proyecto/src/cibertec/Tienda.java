70d3610e5ce9bd0e5dc45b73ad88b9d17fda8f4d
jesus <jesusflsa2004@gmail.com>
2023-10-29 19:58:19 -0500

    Third attempt to try fix this

--- Proyecto/src/cibertec/Tienda.java
+++ Proyecto/src/cibertec/Tienda.java
@@ -30,8 +30,7 @@ public class Tienda extends JFrame implements ActionListener {
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch (Throwable e) {
+		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 		EventQueue.invokeLater(new Runnable() {
@@ -39,8 +38,7 @@ public class Tienda extends JFrame implements ActionListener {
 				try {
 					Tienda frame = new Tienda();
 					frame.setVisible(true);
-				}
-				catch (Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -54,59 +52,64 @@ public class Tienda extends JFrame implements ActionListener {
 		setTitle("Tienda 1.0");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 762, 465);
-		
+
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		mnMantenimiento = new JMenu("Mantenimiento");
 		mnMantenimiento.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		menuBar.add(mnMantenimiento);
-		
+
 		mntmClientes = new JMenuItem("Clientes");
 		mntmClientes.addActionListener(this);
-		
+
 		mntmVendedores = new JMenuItem("Vendedores");
 		mntmVendedores.addActionListener(this);
 		mntmVendedores.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnMantenimiento.add(mntmVendedores);
 		mntmClientes.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnMantenimiento.add(mntmClientes);
-		
+
 		mntmProductos = new JMenuItem("Productos");
 		mntmProductos.addActionListener(this);
 		mntmProductos.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnMantenimiento.add(mntmProductos);
-		
+
 		mnVentas = new JMenu("Ventas");
 		mnVentas.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		menuBar.add(mnVentas);
-		
+
 		mntmVender = new JMenuItem("Vender");
+		mntmVender.addActionListener(this);
 		mntmVender.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnVentas.add(mntmVender);
-		
+
 		mnReportes = new JMenu("Reportes");
 		mnReportes.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		menuBar.add(mnReportes);
-		
+
 		mntmReporteProductos = new JMenuItem("Reporte general por productos");
 		mntmReporteProductos.addActionListener(this);
 		mntmReporteProductos.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnReportes.add(mntmReporteProductos);
-		
+
 		mntmReporteVendedores = new JMenuItem("Reporte general por vendedores");
+		mntmReporteVendedores.addActionListener(this);
 		mntmReporteVendedores.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnReportes.add(mntmReporteVendedores);
-		
+
 		mntmReportePorVendedor = new JMenuItem("Reporte por vendedor");
+		mntmReportePorVendedor.addActionListener(this);
 		mntmReportePorVendedor.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnReportes.add(mntmReportePorVendedor);
-		
+
 		mntmReportePorProducto = new JMenuItem("Reporte por producto");
+		mntmReportePorProducto.addActionListener(this);
 		mntmReportePorProducto.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnReportes.add(mntmReportePorProducto);
-		
+
 		mntmReportePrecios = new JMenuItem("Reporte de precios");
+		mntmReportePrecios.addActionListener(this);
 		mntmReportePrecios.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		mnReportes.add(mntmReportePrecios);
 		contentPane = new JPanel();
@@ -114,6 +117,7 @@ public class Tienda extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 	}
+
 	private JMenuItem mntmProductos;
 	private JMenuItem mntmVendedores;
 	private JMenuItem mntmVender;
@@ -123,53 +127,86 @@ public class Tienda extends JFrame implements ActionListener {
 	private JMenuItem mntmReportePrecios;
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == mntmVendedores) {
-		
+		if (e.getSource() == mntmReportePrecios) {
+			actionPerformedMntmReportePrecios(e);
 		}
-		if (e.getSource() == mntmProductos) {
-			actionPerformed(e);
+		if (e.getSource() == mntmReportePorProducto) {
+			actionPerformedMntmReportePorProducto(e);
+		}
+		if (e.getSource() == mntmReportePorVendedor) {
+			actionPerformedMntmReportePorVendedor(e);
+		}
+		if (e.getSource() == mntmReporteVendedores) {
+			actionPerformedMntmReporteVendedores(e);
 		}
 		if (e.getSource() == mntmReporteProductos) {
-			actionPerformedMntmCons(e);
+			actionPerformedMntmReporteProductos(e);
+		}
+		if (e.getSource() == mntmVender) {
+			actionPerformedMntmVender(e);
+		}
+		if (e.getSource() == mntmProductos) {
+			actionPerformedMntmProductos(e);
 		}
 		if (e.getSource() == mntmClientes) {
-			actionPerformed(e);
+			actionPerformedMntmClientes(e);
 		}
-		
+		if (e.getSource() == mntmVendedores) {
+			actionPerformedMntmVendedores(e);
+		}
 	}
-	
-	protected void actionPerformedMntmCons(ActionEvent e) {
-		//Declarar una variable referencia que permita controlar la caja
-		DialogoVender d;
-		//Crear la caja de diálogo y asignar el control a las variables
-		d = new DialogoVender();
-		//Fijar la localización de la caja de diálogo en relación a la ventana de la tienda
-		d.setLocationRelativeTo(this);
-		//Hacer visible la caja de diálogo
+
+	protected void actionPerformedMntmVendedores(ActionEvent e) {
+		DialogoVendedores d = new DialogoVendedores();
 		d.setVisible(true);
+		d.setLocationRelativeTo(this);
 	}
-	
+
+	protected void actionPerformedMntmClientes(ActionEvent e) {
+		DialogoClientes d = new DialogoClientes();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmProductos(ActionEvent e) {
+		DialogoProductos d = new DialogoProductos();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmVender(ActionEvent e) {
+		DialogoVender d = new DialogoVender();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmReporteProductos(ActionEvent e) {
+		DialogoReporteGeneralProductos d = new DialogoReporteGeneralProductos();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmReporteVendedores(ActionEvent e) {
+		DialogoReportePorVendedor d = new DialogoReportePorVendedor();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmReportePorVendedor(ActionEvent e) {
+		DialogoReportePorVendedor d = new DialogoReportePorVendedor();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmReportePorProducto(ActionEvent e) {
+		DialogoReporteGeneralProductos d = new DialogoReporteGeneralProductos();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
+
+	protected void actionPerformedMntmReportePrecios(ActionEvent e) {
+		DialogoReportePrecios d = new DialogoReportePrecios();
+		d.setVisible(true);
+		d.setLocationRelativeTo(this);
+	}
 }
-	
-	
-
-	
-
-
-
-	
-		
-	
-
-
-	
-
-	
-		
-		
-		
-
-
-
-
- 
